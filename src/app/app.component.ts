ccc5ba66d8d0c02b1d35878361798cbb419c0adf
Welington Moraes <53630451+WelingtonMoraes@users.noreply.github.com>
2022-10-23 23:33:35 -0300

    nova secção de funcionarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, NgModule, ViewEncapsulation } from '@angular/core';
+import { Component, ViewChild, ViewEncapsulation } from '@angular/core';
 // import Swiper core and required modules
 import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { SwiperComponent } from 'swiper/angular';
 // install Swiper modules
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
@@ -8,8 +9,10 @@ SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
+  @ViewChild('swiper', { static: false }) swiper?: SwiperComponent
   title = 'cenarium';
 
   public openTab(_tab: string) {
